0cb57f85243d6f41c6f5975dfe62a3ad4b7247fd
PERCIVAL KUFAMUSARIRA <113292801+peekayofficial@users.noreply.github.com>
2024-08-12 12:05:13 +0200

    First commit

---
+++ ls3meaning.py
@@ -0,0 +1,9 @@
+meaning = 42
+print('')
+# if meaning > 10:
+#     print('Right on')
+# else:
+#     print('Not quite right')
+
+# Tenary Operator
+print('Grade A') if meaning > 35 else print('Grade B')
